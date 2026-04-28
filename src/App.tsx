19d27e2ff984987e306a0f29d1d7c1d83ef0d5c5
Oleksii Kyselov <oleksii.kyselov@globallogic.com>
2026-04-28 16:12:47 +0300

    feat: Add init App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="text-center">
+      <h1 className="underline text-2xl mb-4">React Training</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
