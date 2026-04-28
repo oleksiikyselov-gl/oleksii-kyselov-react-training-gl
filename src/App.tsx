f5e22b677152d672659ac610d7d65181aa9825f6
Oleksii Kyselov <oleksii.kyselov@globallogic.com>
2026-04-28 17:59:06 +0300

    feat: Add radix-ui and test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import { Button } from '@/components/ui/button';
 
 const App = () => {
   return (
     <div className="text-center">
       <h1 className="underline text-2xl mb-4">React Training</h1>
       <p>Start building amazing things with Rsbuild.</p>
+
+      <Button variant="outline">Button</Button>
     </div>
   );
 };
