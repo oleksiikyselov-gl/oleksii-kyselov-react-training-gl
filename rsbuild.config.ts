2d5ba13e9351cdc489958948764ebaaba1a19741
Oleksii Kyselov <oleksii.kyselov@globallogic.com>
2026-05-05 10:42:43 +0300

    feat: add App router and GitHub Pages deployment configuration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,6 +3,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: './',
+  },
   source: {
     // @ts-expect-error unknown error below
     alias: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RegistrationForm from './pages/RegistrationForm';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/registration" element={<RegistrationForm />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
